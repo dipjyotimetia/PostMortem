6419cfe7174967b714b3c88dfff83a5dd507a47a
Dipjyoti Metia <dipjyotimetia@gmail.com>
2025-09-04 22:44:59 +1000

    test: enhance extension tests and improve test runner setup

--- vscode-extension/src/test/suite/index.ts
+++ vscode-extension/src/test/suite/index.ts
@@ -1,3 +1,34 @@
+import * as path from 'path';
+import Mocha from 'mocha';
+import { glob } from 'glob';
+
 export function run(): Promise<void> {
-  return Promise.resolve();
+  // Create the mocha test
+  const mocha = new Mocha({
+    ui: 'tdd',
+    color: true
+  });
+
+  const testsRoot = path.resolve(__dirname, '..');
+
+  return new Promise((resolve, reject) => {
+    glob('**/**.test.js', { cwd: testsRoot }).then(testFiles => {
+      // Add files to the test suite
+      testFiles.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+      try {
+        // Run the mocha test
+        mocha.run((failures: number) => {
+          if (failures > 0) {
+            reject(new Error(`${failures} tests failed.`));
+          } else {
+            resolve();
+          }
+        });
+      } catch (err) {
+        console.error(err);
+        reject(err);
+      }
+    }).catch(reject);
+  });
 }
